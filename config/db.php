fbddadb9d9248da0d0d18c15fb9a3957d555a848
gr441_sisse <m000rtualuna@gmail.com>
2026-05-14 15:30:08 +0700

    check commit

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'uxkviidd_m1',
+    'username' => 'uxkviidd',
+    'password' => 'gZpZrN',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
